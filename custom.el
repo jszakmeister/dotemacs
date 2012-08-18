4f762582cb1c12a9cf88a2c740ede9cedb04b4f5
John Szakmeister <john@szakmeister.net>
2012-08-18 05:23:35 -0400

    Adjust colors of rainbow parens.
    They were a little too subtle for my taste.

--- custom.el
+++ custom.el
@@ -1,13 +1,20 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(normal-erase-is-backspace t)
  '(rst-level-face-base-light 10))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "grey50"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "orange1"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow1"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green1"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "cyan1"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "slateblue1"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "magenta1"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "purple")))))
