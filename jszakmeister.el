d17bfa012fb996bf0e5a13933491f8a812252f9a
John Szakmeister <john@szakmeister.net>
2012-08-23 05:08:35 -0400

    jszakmeister: tab to spaces

--- jszakmeister.el
+++ jszakmeister.el
@@ -163,7 +163,7 @@
 
 ;; Bring in Markdown mode
 (autoload 'markdown-mode "markdown-mode"
-	  "Major mode for editing Markdown files" t)
+          "Major mode for editing Markdown files" t)
 (setq auto-mode-alist
       (cons '("\\.md" . markdown-mode) auto-mode-alist))
 
