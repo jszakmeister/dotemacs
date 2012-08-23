0f7a87c7b179f55d62fc6de0896b5e5553f2e7e3
John Szakmeister <john@szakmeister.net>
2012-08-23 04:54:00 -0400

    init: correctly add the melpa repo
    add-to-list doesn't take a series of arguments to add to the list.  Only
    the second argument is added.  Fix up this usage so that fetching
    packages from melpa works.

--- init.el
+++ init.el
@@ -12,7 +12,8 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/")
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (package-initialize)
