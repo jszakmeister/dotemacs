c619915e316a1c3e791bd537e2fca5e04924b56c
John Szakmeister <john@szakmeister.net>
2012-10-09 06:30:10 -0400

    Drop minor modes from the mode-line and add the evil mode.

--- user/jszakmeister/dotemacs-after.el
+++ user/jszakmeister/dotemacs-after.el
@@ -132,8 +132,80 @@
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "C-x f") 'helm-for-files)
 
+(require 'evil)
+(evil-mode 1)
+
 (require 'powerline)
-(powerline-default)
+
+(defface powerline-normal '((t (:background "green yellow"
+                                :foreground "green4"
+		                :inherit mode-line)))
+  "Powerline face for Normal mode."
+  :group 'powerline)
+
+(defface powerline-visual '((t (:background "DarkOrange1"
+                                :foreground "DarkOrange4"
+		                :inherit mode-line)))
+  "Powerline face for Visual mode."
+  :group 'powerline)
+
+(defun powerline-evil-mode-line-tag ()
+  (cond ((string= " <N> " evil-mode-line-tag) (list
+						(powerline-raw "N " 'powerline-normal 'l)
+                                                (powerline-arrow-right 'powerline-normal nil)))
+	((string= " <I> " evil-mode-line-tag) (list
+					       (powerline-raw "Insert " 'powerline-normal 'l)
+                                               (powerline-arrow-right 'powerline-normal nil)))
+	((string= " <V> " evil-mode-line-tag) (list
+					       (powerline-raw "Visual " 'powerline-visual 'l)
+                                               (powerline-arrow-right 'powerline-visual nil)))
+	(t (list
+	    (powerline-raw evil-mode-line-tag nil 'l)
+            (powerline-arrow-right nil nil)))))
+
+(setq-default mode-line-format
+              '("%e"
+                (:eval
+                 (let* ((active (eq (frame-selected-window) (selected-window)))
+                        (face1 (if active 'powerline-active1 'powerline-inactive1))
+                        (face2 (if active 'powerline-active2 'powerline-inactive2))
+                        (lhs (list
+			      (powerline-raw evil-mode-line-tag 'powerline-normal 'l)
+			      ;(powerline-evil-mode-line-tag)
+                              (powerline-arrow-right 'powerline-normal nil)
+                              (powerline-raw "%*" nil 'l)
+                              (powerline-buffer-size nil 'l)
+                              (powerline-buffer-id nil 'l)
+
+                              (powerline-raw " ")
+                              (powerline-arrow-right nil face1)
+
+                              (powerline-major-mode face1 'l)
+                              (powerline-raw mode-line-process face1 'l)
+
+                              (powerline-arrow-right face1 face2)
+
+                              (powerline-vc face2)
+                              ))
+                        (rhs (list
+                              (powerline-raw global-mode-string face2 'r)
+
+                              (powerline-arrow-left face2 face1)
+
+                              (powerline-raw "%4l" face1 'r)
+                              (powerline-raw ":" face1)
+                              (powerline-raw "%3c" face1 'r)
+
+                              (powerline-arrow-left face1 nil)
+                              (powerline-raw " ")
+
+                              (powerline-raw "%6p" nil 'r)
+
+                              (powerline-hud face2 face1))))
+                   (concat
+                    (powerline-render lhs)
+                    (powerline-fill face2 (powerline-width rhs))
+                    (powerline-render rhs))))))
 
 ;; Fill column indicator
 (require 'fill-column-indicator)
@@ -142,6 +214,3 @@
 (setq fci-rule-width 1)
 (setq fci-rule-color "grey17")
 (setq-default fill-column 78)
-
-(require 'evil)
-(evil-mode 1)
