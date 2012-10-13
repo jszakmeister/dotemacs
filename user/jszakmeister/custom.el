cc6d04e9e13d08f9b4c89a27c9f0a65db8942db0
John Szakmeister <john@szakmeister.net>
2012-10-13 15:54:44 -0400

    jszakmeister: use a smaller font in the mode-line

--- user/jszakmeister/custom.el
+++ user/jszakmeister/custom.el
@@ -5,8 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("23f9d78cdb308d3b5eb3aa6a1e264da86131fb362678331e5e729c394008337e" default)))
  '(helm-allow-skipping-current-buffer t)
- '(helm-c-adaptive-history-file
-    (expand-file-name (concat user-emacs-directory "helm-c-adaptive-history")))
+ '(helm-c-adaptive-history-file (expand-file-name (concat user-emacs-directory "helm-c-adaptive-history")))
  '(helm-c-adaptive-history-length 1000)
  '(helm-c-enable-eval-defun-hack nil)
  '(helm-c-use-adaptative-sorting t)
@@ -17,6 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:background "grey75" :foreground "black" :box nil :height 140))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "grey50"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "orange1"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow1"))))
