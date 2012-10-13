9b0b4b504fb4bc7beb787bf8324cf50c426d2700
John Szakmeister <john@szakmeister.net>
2012-10-13 07:03:26 -0400

    jszakmeister: turn on rainbow delimiters for emacs-lisp-mode

--- user/jszakmeister/dotemacs-after.el
+++ user/jszakmeister/dotemacs-after.el
@@ -110,6 +110,7 @@
 
 (require 'rainbow-delimiters)
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
 ;; Turn off the system beep
 (setq visible-bell t)
