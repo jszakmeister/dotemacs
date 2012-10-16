ba5bafd9a51d052dea1c71a3e7ad85336cf1264f
John Szakmeister <john@szakmeister.net>
2012-10-16 05:38:32 -0400

    init: re-activate marmalade
    There are some packages that we want from there.

--- init.el
+++ init.el
@@ -105,8 +105,8 @@
 
 
 (require 'package)
-;(add-to-list 'package-archives
-;             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/"))
 ;(add-to-list 'package-archives
